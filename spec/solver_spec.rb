9f587fdd8dcc9f4ee95a65fb59263a63b0c67c91
0sugo <josugo26@gmail.com>
2023-06-30 18:42:33 +0300

    Add reverse spec

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,7 +2,7 @@ describe Solver do
     before :each do
         @solve = Solver.new
     end
-    
+
     context 'when solver class is initialize' do
         it 'will return factorial of n' do
             @result = solve.factorial (3)
@@ -13,10 +13,17 @@ describe Solver do
             @result = solve.factorial(0)
             @expected_result = 1
             expect(@result).to eq(@expected_result)
-    end 
+    end
         it 'will return error if arguement is negative' do
             expect{solve.factorial(-1)}.to raise_error('you entered a negative number');
     end
+    context 'test reverse method in solver class' do
+        it 'returns reversed string' do
+          reversed_string = solve.reverse('artur')
+          expected_str = 'rutra'
+          expect(reversed_string).to eq(expected_str)
+        end
+      end
 
 end
-    
+
